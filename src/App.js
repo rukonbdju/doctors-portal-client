e8c428a375b9461de87e49354f21f39deac494d3
rukonbd1 <rukonbd.dev@gmail.com>
2022-11-16 14:21:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  Routes,
+  Route,
+  link
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Navbar from './Pages/Shared/Navbar';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import Navbar from '../Shared/Navbar';
+import Header from './Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
